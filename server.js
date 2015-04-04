b3fe2a539604435131bedf4de862942d113cafb8
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 14:15:46 -0400

    server update

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/pet/:id', function(req, res){
 	var pet = {};
 	
 
-	db.all("SELECT name, imageUrl FROM " + table_name + " WHERE name =" + str[1], function(err, rows) {
+	db.all("SELECT name, imageUrl FROM " + table_name + " WHERE name = " + str[1].toString, function(err, rows) {
 		console.log("SELECT");
 
 		rows.forEach(function (row) {  
