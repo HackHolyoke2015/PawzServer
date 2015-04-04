1c35a7b7acb4bf2267b5d1e9367ccbff81025d58
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:27:15 -0400

    --access db

--- db_access.js
+++ db_access.js
@@ -3,6 +3,7 @@ var sqlite3         =       require('sqlite3').verbose();
 var db              =       new sqlite3.Database('./database_name.db');
 
 //Perform SELECT Operation
-db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+db.all("SELECT * from pet_info",function(err,rows){
+	console.log(rows);
 //rows contain values while errors, well you can figure out.
 });
