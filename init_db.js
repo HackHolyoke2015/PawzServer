c541176460e59cd86207267dcae4c34a33f6f245
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 04:21:49 -0400

    --looping over pets

--- init_db.js
+++ init_db.js
@@ -14,13 +14,25 @@ fs.readFile('./MockData.json', 'utf8', function (err, data) {
         try {
             json = JSON.parse(data);
             console.log("JSON loaded successfully");
-            console.log(json.data);
+            //console.log(json.data);
 
             var mockdata = json.mockdata;
-            console.log(mockdata);
+            //console.log(mockdata);
 
             var pets = mockdata.pets;
-            console.log(pets);
+            //console.log(pets);
+            var pet;
+            var id;
+            var name;
+            var url;
+            for (i = 0; i < pets.length; i++)
+            {
+              pet = pets[i];
+              id = pet.id;
+              name = pet.name;
+              url = pet.url;
+              console.log(i + ": id: " + id + ", name: " + ", url: " + url);
+            }
 
             //var pet = data["mockdata"]["pets"][0];
             //console.log(pet);
