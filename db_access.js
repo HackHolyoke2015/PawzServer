ab3abb5fb454328847a74500dbb74c865ccb4b94
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:37:28 -0400

    --updating name

--- db_access.js
+++ db_access.js
@@ -3,6 +3,6 @@ var sqlite3         =       require('sqlite3').verbose();
 var db              =       new sqlite3.Database('./database_name.db');
 
 
-db.each("SELECT rowid AS id, name, imageUrl FROM " + table_name, function(err, row) {
+db.each("SELECT rowid AS id, name, imageUrl FROM pet_info", function(err, row) {
     console.log(row.id + ": " + row.name + ", " + row.imageUrl);
 });
