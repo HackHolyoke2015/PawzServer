0d968f7888a5f2f3050dea2de25df7f52ad155ab
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:52:10 -0400

    --big FROM

--- db_access.js
+++ db_access.js
@@ -1,9 +1,9 @@
 //Load modules
-var sqlite3         =       require('sqlite3').verbose();
-var db              =       new sqlite3.Database('./database_name.db');
+var sqlite3	= require('sqlite3').verbose();
+var db = new sqlite3.Database('./database_name.db');
 
 
-db.all("SELECT name, imageUrl from pet_info", function(err, rows) {
+db.all("SELECT name, imageUrl FROM pet_info", function(err, rows) {
 	rows.forEach(function (row) {  
         console.log(row.name, row.imageUrl);  
     })
