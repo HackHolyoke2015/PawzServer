d38c7fbc31b9d1f117293943c53f09ac9604b216
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 12:28:02 -0400

    --updating routing

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname + '/PawzClient/NicoleClient' + '/pawss.html'));
 });
 
-app.get('/jquery-1.11.2.min.js',function(req,res){
+app.get('/jquery.min.js',function(req,res){
 	console.log("Getting Test page");
 	res.sendFile(path.join(__dirname + '/PawzClient/NicoleClient' + '/jquery-1.11.2.min.js'));
 });
