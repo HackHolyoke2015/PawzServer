f5c14e57f76d6c93098828f2a0456db0602a1c0e
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:49:58 -0400

    --I think I've cracked it....

--- db_access.js
+++ db_access.js
@@ -3,8 +3,8 @@ var sqlite3         =       require('sqlite3').verbose();
 var db              =       new sqlite3.Database('./database_name.db');
 
 
-db.all("SELECT * from pet_info", function(err, rows) {
-	console.log(rows.toString());
-
-	console.log(rows[0]);
+db.all("SELECT name, imageUrl from pet_info", function(err, rows) {
+	rows.forEach(function (row) {  
+        console.log(row.name, row.imageUrl);  
+    })
 });
