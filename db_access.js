b155b725c304a4e2428093d019376823f2ccbbcc
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:24:03 -0400

    --Success!

---
+++ db_access.js
@@ -0,0 +1,8 @@
+//Load modules
+var sqlite3         =       require('sqlite3').verbose();
+var db              =       new sqlite3.Database('./database_name.db');
+
+//Perform SELECT Operation
+db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+//rows contain values while errors, well you can figure out.
+});
--- init_db.js
+++ init_db.js
@@ -3,7 +3,7 @@ var utils = require('util');
 var path = require('path');
 var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('records.db');
+var db = new sqlite3.Database('./records.db');
 var json;
 
 var table_name = "pet_info";
