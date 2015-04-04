247f586a9ae7a370ac3dc4bdaa48e760fe4be384
Nicole Flokos <nflokos@cogstate.com>
2015-04-04 02:02:42 -0400

    sqllite

--- sql.js
+++ sql.js
@@ -13,6 +13,27 @@ db.serialize(function() {
   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
       console.log(row.id + ": " + row.info);
   });
+  
+});
+
+db.all("SELECT * FROM lorem", function(err, rows){
+	console.log(rows, "HIIII"); 
 });
 
 db.close();
+
+
+
+//Perform SELECT Operation
+//db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+//rows contain values while errors, well you can figure out.
+//});
+
+//Perform INSERT operation.
+//db.run("INSERT into table_name(col1,col2,col3) VALUES (val1,val2,val3)");
+
+//Perform DELETE operation
+//db.run("DELETE * from table_name where condition");
+
+//Perform UPDATE operation
+//db.run("UPDATE table_name where condition");
