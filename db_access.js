1919dd8ccc68a192d62b90c5261dfff32c702852
Nicole Flokos <nflokos@cogstate.com>
2015-04-04 09:22:02 -0400

    using zach's code to get DB read/write

--- db_access.js
+++ db_access.js
@@ -1,10 +1,52 @@
 //Load modules
-var sqlite3	= require('sqlite3').verbose();
-var db = new sqlite3.Database('./database_name.db');
+var sqlite3 = require('sqlite3').verbose();
+var http = require('http');
+var utils = require('util');
+var path = require('path');
+var fs = require('fs');
+var json;
+var db = new sqlite3.Database('./pets.db'); // name of database is pets :3
+
+
+db.run("CREATE TABLE if not exists pet_info (info TEXT)"); //create table pet_info
+
+
+fs.readFile('MockData.json', 'utf8', function (err, data) {
+    console.log("JSON Initializing...");
+    if (err) {
+        console.log("ERROR: JSON load - " + err);
+        throw err;
+    } else {
+        try {
+            json = JSON.parse(data);
+            console.log("JSON loaded successfully");
+            //console.log(json.data);
+
+            var mockdata = json.mockdata;
+            console.log(mockdata);
+            pets = mockdata.pets
+            // for (i = 0; i < pets.length; i++)
+            // {
+            //   pet = pets[i];
+            //   id = pet.id;
+            //   name = pet.name;
+            //   url = pet.url;
+            //   console.log(i + ": id: " + id + ", name: " + ", url: " + url);
+            // }
+
+            //var pet = data["mockdata"]["pets"][0];
+            //console.log(pet);
+        }
+        catch (ex) {
+            console.log("ERROR: JSON parse - " + err);
+        }
+    }
+    console.log("JSON initialized!");
+});
 
 
 db.all("SELECT name, imageUrl FROM pet_info", function(err, rows) {
-	rows.forEach(function (row) {  
-        console.log(row.name, row.imageUrl);  
-    })
+	//rows.forEach(function (row) {  
+    //    console.log(row.name, row.imageUrl);  
+    //})
 });
--- sql.js
+++ sql.js
@@ -2,21 +2,30 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('abc.db');
 var check;
 db.serialize(function() {
+    var records = [
+        {petID: 1,name:'george', url:'facebook.com'},
+        {petID: 2,name:'bernice', url:'facebook.com'},
+        {petID: 3,name:'jonathon', url:'facebook.com'},
+        {petID: 4,name:'crystal', url:'facebook.com'},
+        {petID: 5,name:'nicole', url:'facebook.com'},
+        {petID: 6,name:'zach', url:'facebook.com'},
+        {petID: 7,name:'greg', url:'facebook.com'},
+    ];
 
-  db.run("CREATE TABLE if not exists lorem (info TEXT)");
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
+  db.run("CREATE TABLE if not exists pet (info TEXT)");
+  var stmt = db.prepare("INSERT INTO pet(petID,name,url) VALUES (?,?,?)");
+  for (var i = 0; i <= records.length; i++) {
+      stmt.run(records[i].petID, records[i].name, records[i].url);
   }
   stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+  //db.each("SELECT rowid AS id, info FROM pet", function(err, row) {
+  //    console.log(row.id + ": " + row.info);
+  //});
   
 });
 
-db.all("SELECT * FROM lorem", function(err, rows){
+db.all("SELECT * FROM pet", function(err, rows){
 	console.log(rows, "HIIII"); 
 });
 
