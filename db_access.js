ea019efecae7b9eba7f7fc392cf9bf86ad6bd67a
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 09:49:15 -0400

    --not using pet_info

--- db_access.js
+++ db_access.js
@@ -4,7 +4,7 @@ var db = new sqlite3.Database('./records.db');
 
 var table_name = "pet_info";
 
-db.all("SELECT name,imageUrl FROM " + pet_info, function(err, rows) {  
+db.all("SELECT name,imageUrl FROM " + table_name, function(err, rows) {  
         rows.forEach(function (row) {  
             console.log(row.name, row.imageUrl);  
         })  
