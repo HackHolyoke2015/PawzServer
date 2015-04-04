939f00a07214406c3fbafd01c6ba493be9634282
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 04:14:46 -0400

    --more json tests

--- init_db.js
+++ init_db.js
@@ -5,20 +5,23 @@ var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
-var obj;
+var json;
 fs.readFile('./MockData.json', 'utf8', function (err, data) {
     if (err) {
         console.log("ERROR: JSON load - " + err);
         throw err;
     } else {
         try {
-            obj = JSON.parse(data);
+            json = JSON.parse(data);
             console.log("JSON loaded successfully");
-            console.log(obj.data);
+            console.log(json.data);
+
+            var mockdata = json.mockdata;
+
+            console.log(mockdata);
+
             //var pet = data["mockdata"]["pets"][0];
             //console.log(pet);
-
-	    console.log(data);
         }
         catch (ex) {
             console.log("ERROR: JSON parse - " + err);
