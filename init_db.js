1fa3779680acab65c3b39a32c959edd1aa1cdb7b
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 04:48:26 -0400

    --messing with positioning

--- init_db.js
+++ init_db.js
@@ -14,41 +14,41 @@ var id;
 var name;
 var url;
 
+fs.readFile('./MockData.json', 'utf8', function (err, data) {
+  if (err) {
+      console.log("ERROR: JSON load - " + err);
+      throw err;
+  } else {
+      try {
+          json = JSON.parse(data);
+          console.log("JSON loaded successfully");
+          //console.log(json.data);
+
+          var mockdata = json.mockdata;
+          //console.log(mockdata);
+          pets = mockdata.pets
+          // for (i = 0; i < pets.length; i++)
+          // {
+          //   pet = pets[i];
+          //   id = pet.id;
+          //   name = pet.name;
+          //   url = pet.url;
+          //   console.log(i + ": id: " + id + ", name: " + ", url: " + url);
+          // }
+
+          //var pet = data["mockdata"]["pets"][0];
+          //console.log(pet);
+      }
+      catch (ex) {
+          console.log("ERROR: JSON parse - " + err);
+      }
+  }
+});
+
 db.serialize(function() {
   console.log("Database Serialization Initializing...");
 
-  fs.readFile('./MockData.json', 'utf8', function (err, data) {
-    if (err) {
-        console.log("ERROR: JSON load - " + err);
-        throw err;
-    } else {
-        try {
-            json = JSON.parse(data);
-            console.log("JSON loaded successfully");
-            //console.log(json.data);
-
-            var mockdata = json.mockdata;
-            //console.log(mockdata);
-            pets = mockdata.pets
-            // for (i = 0; i < pets.length; i++)
-            // {
-            //   pet = pets[i];
-            //   id = pet.id;
-            //   name = pet.name;
-            //   url = pet.url;
-            //   console.log(i + ": id: " + id + ", name: " + ", url: " + url);
-            // }
-
-            //var pet = data["mockdata"]["pets"][0];
-            //console.log(pet);
-        }
-        catch (ex) {
-            console.log("ERROR: JSON parse - " + err);
-        }
-
-
-    }
-  });
+  
 
   db.run("CREATE TABLE if not exists " + table_name + " (id INTEGER PRIMARY KEY, name TEXT, imageUrl TEXT)");
   var stmt = db.prepare("INSERT INTO " + table_name + "(id,name,imageUrl) VALUES (?,?,?)");
