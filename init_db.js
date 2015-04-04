ab3968215a697b622054b36fabcbb67560dc61bc
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-04 05:12:39 -0400

    --updating table name

--- init_db.js
+++ init_db.js
@@ -6,7 +6,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('records.db');
 var json;
 
-var table_name = "user_info";
+var table_name = "pet_info";
 var pets;
 //console.log(pets);
 var pet;
